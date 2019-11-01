3fd773f62924045e57c2a6f151ee7593cf7b601e
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-01 21:32:14 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
 # Your Code Here
+require 'pry'
+
+def map(source_array)
+  new = []
+  source_array.each do |index|
+    new << yield(index)
+  end
+new
+end
+
+def reduce(source_array, starting_point = false)
+  if starting_point
+    sv = starting_point
+    sum = sv
+    i = 0
+  else
+    sum = source_array[0]
+    i = 1
+  end
+  
+  while i < source_array.length
+    sum  = yield(sum, source_array[i])
+    i += 1
+  end
+
+sum
+end
