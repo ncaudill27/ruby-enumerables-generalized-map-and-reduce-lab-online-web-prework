3b52ed1eb09bcabe55722f87c612bb70917d4f17
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-01 21:25:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,10 +13,8 @@ def reduce(source_array, starting_point = false)
   if starting_point
     sv = starting_point
     sum = sv
-    i = 0
   else
     sum = source_array[0]
-    i = 1
   end
   
   source_array.each do |i|
