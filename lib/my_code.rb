8b8fc092a33b6dddfd333a30723d1d7fac516064
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-01 20:47:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,18 @@
 # Your Code Here
+require 'pry'
+
+def map(source_array)
+  new = []
+  source_array.each do |index|
+    new << yield(index)
+  end
+new
+end
+
+def reduce(source_array, starting_point = 0)
+  value = starting_point
+  source_array.each do |index|
+    yield(value)
+  end
+value
+end
